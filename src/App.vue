5c23fb2984211d1123438c6028a8553ecd84d1ce
imada4911 <78717070+imada4911@users.noreply.github.com>
2021-04-30 00:56:32 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <p>{{ number }}</p>
+    <button @click="add">追加する</button>
+    <button @click="remove">減らす</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.number = this.number + 1;
+    },
+    remove() {
+      this.number = this.number - 1;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
